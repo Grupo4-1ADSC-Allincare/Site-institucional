805d98a40745a4ff6bf909c957f23b75e0fed6ae
Maryanna Silva <maryanna.silva@sptech.school>
2022-05-30 16:26:10 -0300

    Tentando conectar API do arduíno

--- Site-oficial-/src/models/medidaModel.js
+++ Site-oficial-/src/models/medidaModel.js
@@ -1,12 +1,12 @@
-var database = require("../database/config");
+/* var database = require("../database/config");
 
 function buscarUltimasMedidas(idVeiculo, limite_linhas) {
     instrucaoSql = `select 
                         valorTemperatura,
                         dataHora,
                         DATE_FORMAT(dataHora,'%H:%i:%s') as momento_grafico
-                    from HistMedida
-                    where fkVeiculo = ${idVeiculo}
+                        from HistMedida
+                        where fkVeiculo = ${idVeiculo}
                     order by idHistMedida desc limit ${limite_linhas}`;
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
@@ -20,6 +20,70 @@ function buscarMedidasEmTempoReal(idVeiculo) {
                         fkVeiculo 
                         from HistMedida where fkVeiculo = ${idVeiculo} 
                     order by idHistMedida desc limit 1`;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    buscarUltimasMedidas,
+    buscarMedidasEmTempoReal
+} */
+
+var database = require("../database/config");
+
+function buscarUltimasMedidas(idVeiculo, limite_linhas) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top ${limite_linhas}
+        valorTemperatura as temperatura,  
+                        dataHora,
+                        CONVERT(varchar, dataHora, 108) as momento_grafico
+                    from HistMedida
+                    where fkVeiculo = ${idVeiculo}
+                    order by idHistMedida  desc`;
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+        valorTemperatura as temperatura,
+                        dataHora,
+                        DATE_FORMAT(dataHora,'%H:%i:%s') as momento_grafico
+                    from HistMedida
+                    where fkVeiculo = ${idVeiculo}
+                    order by idHistMedida desc limit ${limite_linhas}`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasEmTempoReal(idVeiculo) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top 1
+        valorTemperatura as temperatura,  
+                        CONVERT(varchar, dataHora, 108) as momento_grafico, 
+                        fkVeiculo
+                        from HistMedida where fkVeiculo = ${idVeiculo} 
+                    order by idHistMedida desc`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `select 
+        valorTemperatura as temperatura,
+                        DATE_FORMAT(dataHora,'%H:%i:%s') as momento_grafico, 
+                        fkVeiculo 
+                        from HistMedida where fkVeiculo = ${idVeiculo} 
+                    order by idHistMedida desc limit 1`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
--- Site-oficial-/src/routes/medidas.js
+++ Site-oficial-/src/routes/medidas.js
@@ -1,3 +1,18 @@
+/* var express = require("express");
+var router = express.Router();
+
+var medidaController = require("../controllers/medidaController");
+
+router.get("/ultimas/:idVeiculo", function (req, res) {
+    medidaController.buscarUltimasMedidas(req, res);
+});
+
+router.get("/tempo-real/:idVeiculo", function (req, res) {
+    medidaController.buscarMedidasEmTempoReal(req, res);
+})
+
+module.exports = router; */
+
 var express = require("express");
 var router = express.Router();
 
