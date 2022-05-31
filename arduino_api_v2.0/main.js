9dcb80047b389b5d72cb0b76baed4b94889456d7
Maryanna Silva <maryanna.silva@sptech.school>
2022-05-30 22:53:56 -0300

    API do arduíno

---
+++ arduino_api_v2.0/main.js
@@ -0,0 +1,81 @@
+const serialport = require('serialport');
+const express = require('express');
+const mysql = require('mysql2');
+
+const SERIAL_BAUD_RATE = 9600;
+const SERVIDOR_PORTA = 3000;
+const HABILITAR_OPERACAO_INSERIR = true;
+
+const serial = async (
+    valoresLm35Temperatura
+) => {
+    const poolBancoDados = mysql.createPool(
+        {
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: 'sptech',
+            database: 'Allincare'
+        }
+    ).promise();
+
+    const portas = await serialport.SerialPort.list();
+    const portaArduino = portas.find((porta) => porta.vendorId == 2341 && porta.productId == 43);
+    if (!portaArduino) {
+        throw new Error('O arduino não foi encontrado em nenhuma porta serial');
+    }
+    const arduino = new serialport.SerialPort(
+        {
+            path: portaArduino.path,
+            baudRate: SERIAL_BAUD_RATE
+        }
+    );
+    arduino.on('open', () => {
+        console.log(`A leitura do arduino foi iniciada na porta ${portaArduino.path} utilizando Baud Rate de ${SERIAL_BAUD_RATE}`);
+    });
+    arduino.pipe(new serialport.ReadlineParser({ delimiter: '\r\n' })).on('data', async (data) => {
+        const valores = data.split(';');
+        const lm35Temperatura = parseFloat(valores[2]);
+
+        valoresLm35Temperatura.push(lm35Temperatura);
+
+        if (HABILITAR_OPERACAO_INSERIR) {
+            await poolBancoDados.execute(
+                'INSERT INTO HistMedida (fkVeiculo, valorTemperatura) VALUES (2, ?)',
+                [lm35Temperatura]
+            );
+            console.log("Valores inseridos no banco: " + lm35Temperatura)
+        }
+
+    });
+    arduino.on('error', (mensagem) => {
+        console.error(`Erro no arduino (Mensagem: ${mensagem}`)
+    });
+}
+
+const servidor = (
+    valoresLm35Temperatura
+) => {
+    const app = express();
+    app.use((request, response, next) => {
+        response.header('Access-Control-Allow-Origin', '*');
+        response.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept');
+        next();
+    });
+    app.listen(SERVIDOR_PORTA, () => {
+        console.log(`API executada com sucesso na porta ${SERVIDOR_PORTA}`);
+    });
+    app.get('/sensores/lm35/temperatura', (_, response) => {
+        return response.json(valoresLm35Temperatura);
+    });
+}
+
+(async () => {
+    const valoresLm35Temperatura = [];
+    await serial(
+        valoresLm35Temperatura
+    );
+    servidor(
+        valoresLm35Temperatura
+    );
+})();
